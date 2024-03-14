0b9499447e651fbb36a57e22939acd6545a44a93
phongdinta <136043782+phongdinta@users.noreply.github.com>
2024-03-14 07:44:32 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("Love story", "This is a song about love", "MP3-001", 100 , 300);
 
-        MP3 mp3;
+        Book book = new Book("I am gifted, so are you!", "Share about author's experience", "BOOK-001", 50, "Adam Khoo", 400, "Psychology");
+
+        System.out.println("MP3 Information:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println("\nBook Information:");
         book.showInfo();
-
     }
 }
