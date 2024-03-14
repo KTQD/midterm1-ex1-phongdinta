9923cca6aec188958cb48ac24c1ab12d8c7e4c03
phongdinta <136043782+phongdinta@users.noreply.github.com>
2024-03-14 07:45:28 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,23 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    float price;
+
+    public Item(String name, String description, String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void showInfo();
 }
