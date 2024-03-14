ca42288dae64ec057340e1fa06d8a97ee5a482f2
phongdinta <136043782+phongdinta@users.noreply.github.com>
2024-03-14 07:45:05 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,25 @@
-public class MP3 {
+public class MP3 extends Item {
+    private int duration;
+    
+    public MP3(String name, String description, String ID, float price, int duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+        System.out.println("Duration: " + duration);
+    }
 }
